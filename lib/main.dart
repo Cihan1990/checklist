fce0285331f68e0b1b1602191a11fc4c13c1d13a
Cihan1990 <cihan.52@hotmail.de>
2024-06-05 11:25:53 +0200

    main dart angepasst

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,84 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MainApp());
+  runApp(Checklist());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Checklist extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Checkliste',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScren(),
+    );
+  }
+}
+
+class MainScren extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScren> {
+  final List<String> _items = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _addItem() {
+    setState(() {
+      _items.add(_controller.text);
+      _controller.clear();
+    });
+  }
+
+  void _removeItem(int index) {
+    setState(() {
+      _items.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Liste'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Gib einen Text ein',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _addItem,
+              child: Text('Hinzufügen'),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_items[index]),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () => _removeItem(index),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
